97c787d4c3eff9523033bdf84d8f6de76805ca7f
abhinavupadhyay2026 <abhinavupadhyay2026@gmail.com>
2026-08-17 18:15:56 +0530

    added few changes

--- environment/preprod/main.tf
+++ environment/preprod/main.tf
@@ -2,15 +2,15 @@
 
 module "resource_group" {
 
-    source = "../../modules/azurerm_resource_group"
-    rg = var.m_rg
-  
+  source = "../../modules/azurerm_resource_group"
+  rg     = var.m_rg
+
 }
 
 module "storage_account" {
-    depends_on = [ module.resource_group ]
-    source = "../../modules/azurerm_storage_account"
-    storage = var.m_stg
+  depends_on = [module.resource_group]
+  source     = "../../modules/azurerm_storage_account"
+  storage    = var.m_stg
 
-  
+
 }
